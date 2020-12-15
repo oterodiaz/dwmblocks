91534fde683a38a30b9e3f861eca3c03a0d13bf5
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2020-12-15 23:12:07 +0100

    Update script name

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 static const Block blocks[] = {
 	/*Icon*/	        /*Command*/	 	                 /*Update Interval*/  /*Update Signal*/
     {"   ", "/home/diego/Scripts/dwmblocks/kernel",		    360,		        2},
-	{" 祥 ", "/home/diego/Scripts/dwmblocks/upt",		        60,		            2},
+	{" 祥 ", "/home/diego/Scripts/dwmblocks/uptime",	        60,		            2},
 	{" ﬙ ",  "/home/diego/Scripts/dwmblocks/memory",	        6,		            1},
 	{" 﨎 ", "/home/diego/Scripts/dwmblocks/temp",  	        6,		            3},
 	{"",     "/home/diego/Scripts/dwmblocks/volume",			0,		            10},
