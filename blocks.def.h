04aea30e5628a8b9f39695ee4a91e83e7486a4ad
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-01-11 18:02:34 +0100

    Update paths

--- blocks.def.h
+++ blocks.def.h
@@ -1,12 +1,12 @@
 static const Block blocks[] = {
 	/*Icon*/	        /*Command*/	 	                 /*Update Interval*/  /*Update Signal*/
-    {"   ", "/home/diego/Scripts/dwmblocks/kernel",		    360,		        2},
-	{" 祥 ", "/home/diego/Scripts/dwmblocks/uptime",	        60,		            2},
-	{" ﬙ ",  "/home/diego/Scripts/dwmblocks/memory",	        6,		            1},
-	{" 﨎 ", "/home/diego/Scripts/dwmblocks/temp",  	        6,		            3},
-	{"",     "/home/diego/Scripts/dwmblocks/volume",			0,		            10},
-	{"",     "/home/diego/Scripts/dwmblocks/brightness",    	0,		            11},
-	{"  ",  "/home/diego/Scripts/dwmblocks/clock",	    		5,		            0},
+    {"   ", "/home/diego/.scripts/dwmblocks/kernel",		    360,		        2},
+	{" 祥 ", "/home/diego/.scripts/dwmblocks/uptime",	        60,		            2},
+	{" ﬙ ",  "/home/diego/.scripts/dwmblocks/memory",	        6,		            1},
+	{" 﨎 ", "/home/diego/.scripts/dwmblocks/temp",  	        6,		            3},
+	{"",     "/home/diego/.scripts/dwmblocks/volume",			0,		            10},
+	{"",     "/home/diego/.scripts/dwmblocks/brightness",    	0,		            11},
+	{"  ",  "/home/diego/.scripts/dwmblocks/clock",    		5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
