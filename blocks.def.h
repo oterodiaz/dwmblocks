3e9b157e797aaf185e91f50f2b88dee1ce247a61
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-02-04 22:39:21 +0100

    Change kernel icon from Gentoo logo to Tux

--- blocks.def.h
+++ blocks.def.h
@@ -1,6 +1,6 @@
 static const Block blocks[] = {
 	/*Icon*/	        /*Command*/	 	                 /*Update Interval*/  /*Update Signal*/
-    {"   ", "/home/diego/.scripts/dwmblocks/kernel",		    360,		        2},
+    {"   ", "/home/diego/.scripts/dwmblocks/kernel",		    360,		        2},
 	{" 祥 ", "/home/diego/.scripts/dwmblocks/uptime",	        60,		            2},
 	{" ﬙ ",  "/home/diego/.scripts/dwmblocks/memory",	        6,		            1},
 	{" 﨎 ", "/home/diego/.scripts/dwmblocks/temp",  	        6,		            3},
