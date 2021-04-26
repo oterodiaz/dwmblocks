9d53412b3f41d9614098549974e7db55e100a04c
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-04-26 18:43:27 +0200

    Add spacing for the brightness module

--- blocks.def.h
+++ blocks.def.h
@@ -6,7 +6,7 @@ static const Block blocks[] = {
 	{" ﬙ ",  "/home/diego/.scripts/dwmblocks/memory",	        6,		            1},
 	{" 﨎 ", "/home/diego/.scripts/dwmblocks/temp",  	        6,		            3},
 	{"",     "/home/diego/.scripts/dwmblocks/volume_pamixer",   0,		            10},
-	{"",     "/home/diego/.scripts/dwmblocks/brightness",    	0,		            11},
+	{" ",     "/home/diego/.scripts/dwmblocks/brightness",    	0,		            11},
 	{"  ",  "/home/diego/.scripts/dwmblocks/clock",    		5,		            0},
 };
 
