f7d218eef45898dd5bccf58885e19096584c1b99
oterodiaz <44239644+oterodiaz@users.noreply.github.com>
2021-09-02 19:11:22 +0200

    Update script names

--- blocks.def.h
+++ blocks.def.h
@@ -5,9 +5,9 @@ static const Block blocks[] = {
     {"   ", "/home/diego/.scripts/dwmblocks/kernel",           360,                  2},
     {" 祥 ",  "/home/diego/.scripts/dwmblocks/uptime",            60,                   2},
     {" ﬙ ",  "/home/diego/.scripts/dwmblocks/memory",           6,                    1},
-    {" 﨎 ",  "/home/diego/.scripts/dwmblocks/temp",              6,                    3},
+    {" 﨎 ",  "/home/diego/.scripts/dwmblocks/temperature",      6,                    3},
     {"",     "/home/diego/.scripts/dwmblocks/volume_pamixer",   0,                    10},
-    {" ",    "/home/diego/.scripts/dwmblocks/brightness",      0,                    11},
+    {" ",    "/home/diego/.scripts/dwmblocks/brightness_icon",   0,                    11},
     {"  ",  "/home/diego/.scripts/dwmblocks/clock",            5,                    0},
 };
 
